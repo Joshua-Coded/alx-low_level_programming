33110d58277cbba14659a4b71b15de84e4ea7892
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-06-26 03:53:57 -0700

    6th task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *
+ *main -Entery-point
+ * 
+ *Return (0) success
+ */
+int main(void)
+{
+   printf("with proper grammer, but the outcome is a piece of art\n");
+   return (0);
+}
