750f835f7a1b3d53c56467e427e114c951cf7dfb
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-01 16:49:29 -0700

    my 6th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints integers
+ * Description: all int are under 10
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+
+	printf("\n");
+	return (0);
+}
