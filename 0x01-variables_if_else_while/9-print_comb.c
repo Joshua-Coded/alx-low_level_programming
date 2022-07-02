9a8bacf591750236b31b5748631ccabcf5b218e6
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-01 17:13:21 -0700

    my 10th task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Description: numbers under 10 with a
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
