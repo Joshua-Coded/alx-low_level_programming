6f361cb7d7abfb424eb1fa292405cde22c6b7b96
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-05 16:26:05 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
-
-
 #include "main.h"
 
 /**
- *_islower - a quick description.
- *@c: contains value to be compared
- *Return: Always 0.
+ * _islower - Short description.
+ * @c: contains value to be compared
+ * Return: Always 0.
  */
 int _islower(int c)
 
@@ -13,10 +11,11 @@ int _islower(int c)
 
 if (c > 'a' && c < 'z')
 {
-return(1);
+return (1);
 }
 
-else {
+else
+{
 return (0);
 }
 
