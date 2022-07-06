b060d5a92a2920d3722d984cdc115ff06159f0fe
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-06 03:23:52 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *main - main
- *Return: 0
- */
+* main - main
+* Return: 0
+*/
 
 int main(void)
 {
 int i;
 i = 0;
 
-postive_or_negative(i);
+positive_or_negative(i);
 
 return (0);
 
