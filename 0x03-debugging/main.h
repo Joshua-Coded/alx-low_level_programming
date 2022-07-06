88670af3512eddd2f82184a6e6ac7d7bef5144b1
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-06 03:52:11 -0700

    dubug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 void convert_day(int month, int day);
 
