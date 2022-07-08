351a710bf961cb82d089dbe13c3e29a007e71984
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-07 20:16:21 -0700

    final task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,9 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void convert_day(int month, int day);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-#endif
+
+
+#endif /* MAIN_H */
+Footer
