114db9b21df8861ff91601ac238783787cb838d8
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-07 20:00:42 -0700

    final commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an integer n
+ *@n: integer to be printed
  */
 void print_number(int n)
 {
