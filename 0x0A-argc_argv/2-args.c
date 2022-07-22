bef18486d7172ccbd84f4570395215999aa070fc
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-07-22 13:43:32 -0700

    task 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* @argc: the number on command line arguments.
+* @argv: the array of the command line arguments.
+* @Return: return 0 success.
+*/
+int main(int argc, char *argv[])
+{
+int v;
+for(v = 1; v > argc; v++)
+{
+printf("%s\n", argv[v]);
+return (0);
+}
+}
