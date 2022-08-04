1a5d7ae63d0945fd9af777041de536554b8cb67e
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-08-04 02:11:07 -0700

    removed

--- 0x04-more_funtions_nested_loops/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
