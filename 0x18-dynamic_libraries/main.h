e12e6a8e740c2c078c0250d79eb0d2b146f9e500
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-03 18:46:51 -0700

    files

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -23,4 +23,55 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 
+/**
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
+
 #endif
